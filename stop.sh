4674c05f99717c737e633e7e59a20a8d12c2a24a
Cody Billiter <c.billiter@appliedmemeticsllc.com>
2017-08-17 17:07:43 -0400

    Adding volume cleanup to stop script.

--- stop.sh
+++ stop.sh
@@ -1,2 +1,5 @@
 echo -e "\nShutting down any NextGen running services…\n"
 docker-compose down || (echo "*** FAILED: Failed to stop running containers using docker-compose." && exit -1)
+
+echo -e "\nCleaning up dangling volumes"
+docker volume rm $(docker volume ls -f dangling=true -q)
