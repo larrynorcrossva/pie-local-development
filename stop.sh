23425b658691fbf71c9adf22cf299a4f4f3323c6
Doug Kurucz <doug.kurucz@ablevets.com>
2018-05-10 10:18:09 -0400

    add stop script, move some services to staff, clean up fixtures, remove services not used by user services 1.6.19

--- stop.sh
+++ stop.sh
@@ -1,31 +1,63 @@
+#!/usr/bin/env bash
 
-if [ -z $1 ] || [ "$1" == "all" ]; then
-    echo -e "\nShutting down NextGen platform services...\n"
-    docker-compose down || (echo "*** FAILED: Failed to stop running containers using docker-compose." && exit -1)
+function stop_core(){
+    docker-compose down
+}
 
-    echo -e "\nCleaning up dangling volumes"
-    docker volume rm $(docker volume ls -f dangling=true -q)
-fi
-
-if [ "$1" == "fixtures" ] || [ "$1" == "all" ]; then
-    echo -e "\nShutting down fixture services...\n"
+function stop_fixtures(){
     docker-compose -f docker-compose-fixtures.yml down
-fi
+}
 
-if [ "$1" == "shared-services" ] || [ "$1" == "all" ]; then
-    echo -e "\nShutting down shared services...\n"
+function stop_data_seed(){
+    docker-compose -f docker-compose-data-seed.yml down
+}
+
+function stop_shared_services(){
     docker-compose -f docker-compose-shared-services.yml down
-fi
+}
 
-if [ "$1" == "var" ] || [ "$1" == "all" ]; then
-    echo -e "\nShutting down var services and apps...\n"
+function stop_vet(){
+    docker-compose -f docker-compose-services-vet.yml down
+}
+
+function stop_staff(){
+    docker-compose -f docker-compose-services-staff.yml down
+}
+
+function stop_var(){
     docker-compose -f docker-compose-var.yml down
-fi
+}
 
-if [ "$1" == "sm" ] || [ "$1" == "all" ]; then
-    echo -e "\nShutting down scheduling manager services and apps...\n"
+function stop_sm(){
     docker-compose -f docker-compose-sm.yml down
-fi
+}
+
+function stop_all(){
+    stop_core;
+    stop_fixtures;
+    stop_data_seed;
+    stop_shared_services;
+    stop_vet;
+    stop_staff;
+    stop_var;
+    stop_sm;
+    stop_all;
+}
+
+while [[ "$#" > 0 ]]; do case $1 in
+  a|all|-a|--all) stop_all; shift;;
+  c|core|-c|--core) stop_core; shift;;
+  f|fixtures|-f|--fixtures) stop_fixtures; shift;;
+  s|ss|-s|--shared-services) stop_shared_services; shift;;
+  v|vet|-v|--vet) stop_vet; shift;;
+  staff|--staff) stop_staff; shift;;
+  var|--var) stop_var; shift;;
+  sm|--sm) stop_sm; shift;;
+  *) echo "Unknown parameter: $1"; exit 1;;
+esac; shift; done
+
+echo -e "\nCleaning up dangling volumes"
+docker volume rm $(docker volume ls -f dangling=true -q)
 
 # removes cache file created by docker from var-oracle-db-mock
 echo -e "\nCleaning up generated docker cache file from oracle-db"
