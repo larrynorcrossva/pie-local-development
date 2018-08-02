28a3e510fc9561fd86514f3d2e2fdd065bbc39a6
Patrick Liesveld <patrick.liesveld@ablevets.com>
2018-08-02 14:50:43 -0400

    VAR-12490: WIP - adds appointment-service and dependencies

---
+++ composed-services.sh
@@ -0,0 +1,21 @@
+# Services are organized into buckets.  When maintained in a single location, the other service scripts can source this file to incorporate changes.
+
+fixtures=(mock-roa var-mongo-db-mock var-oracle-db-mock video-visits-mongo-mock vvs-mongo-mock)
+
+via=(via-mock-engine via-oracle via-vista via-weblogic)
+
+core=(registrator apigateway consul user-services mock-mvi mock-mvi-ablevets wayf-web redis )
+
+utilities=(swagger-ui zipkin)
+
+ss=(var-messaging-microservice facility-service vmr-mock-service video-visits-service messaging-publisher scheduling-service mobile-appointment-service)
+# removed task-resources
+
+vet=(roa-services roa-web eula-service eula-web veteran-video-connect-service iamssoe-proxy-mock iamssoe-db-mock)
+
+staff=(patient-context mock-wstrust ssoi-mock sud-service sud-web pcs-web personal-preference-service vista-data-services mdws-via-adapter staff-video-connect-service)
+
+var=(var-resources var-web)
+
+sm=(scheduling-manager-resources scheduling-manager-web)
+
