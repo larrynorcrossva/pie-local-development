7f489c608bba1eecfe106a1c4e421890e8da4101
Vidya Chougule <Vidya.Chougule@ablevets.com>
2018-10-03 14:53:02 -0400

    VAR-13224 changes related to code review feeback from Patrick

--- composed-services.sh
+++ composed-services.sh
@@ -1,18 +1,18 @@
 # Services are organized into buckets.  When maintained in a single location, the other service scripts can source this file to incorporate changes.
 
-fixtures=(mock-roa var-mongo-db-mock var-oracle-db-mock video-visits-mongo-mock vvs-mongo-mock)
+fixtures=(mock-roa var-mongo-db-mock var-oracle-db-mock video-visits-mongo-mock vvs-mongo-mock mock-adr)
 
 core=(registrator apigateway user-services mock-mvi wayf-web authorization-rules-service redis)
 
 utilities=(swagger-ui zipkin)
 
-ss=(var-messaging-microservice facility-service vmr-mock-service video-visits-service messaging-publisher vista-scheduling-service appointment-service cdw-service task-resources)
+ss=(var-messaging-microservice facility-service vmr-mock-service video-visits-service messaging-publisher vista-scheduling-service appointment-service cdw-service task-resources adr-service)
 
 vet=(roa-services roa-web eula-service eula-web veteran-video-connect-service iamssoe-proxy-mock iamssoe-db-mock)
 
 staff=(patient-context mock-wstrust ssoi-mock sud-service sud-web pcs-web personal-preference-service vista-data-services mdws-via-adapter staff-video-connect-service)
 
-var=(var-resources var-web mock-adr adr-service)
+var=(var-resources var-web)
 
 sm=(scheduling-manager-resources scheduling-manager-web)
 
