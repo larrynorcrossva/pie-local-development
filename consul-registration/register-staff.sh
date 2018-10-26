3b5088edf7ed931a6dde7a06e34141c877035cae
Patrick Liesveld <patrick.liesveld@ablevets.com>
2018-10-26 19:22:18 -0400

    adds emr-service 2.2.0

--- consul-registration/register-staff.sh
+++ consul-registration/register-staff.sh
@@ -13,3 +13,6 @@ echo "********************* Register NextGen consul staff-videoconnect-resources
 curl -H "X-Consul-Token: ${CONSUL_MASTER_TOKEN}" -X PUT -d "http://video-visits-service:8080/video-visit-resources/v2" http://${HOST_DOMAIN}:8500/v1/kv/appconfig/${VAMF_ENVIRONMENT}/staff-video-connect-service/APP_SVC_VIDEO_VISITS_SERVICE_URL >/dev/null
 curl -H "X-Consul-Token: ${CONSUL_MASTER_TOKEN}" -X PUT -d "video-visits" http://${HOST_DOMAIN}:8500/v1/kv/appconfig/${VAMF_ENVIRONMENT}/staff-video-connect-service/APP_SVC_MONGO_SVC_DATABASE_NAME >/dev/null
 curl -H "X-Consul-Token: ${CONSUL_MASTER_TOKEN}" -X PUT -d "mongodb://video-visits:1234@video-visits-mongo-mock:27017/video-visits?maxPoolSize=20&ssl=false" http://${HOST_DOMAIN}:8500/v1/kv/appconfig/${VAMF_ENVIRONMENT}/staff-video-connect-service/APP_SVC_MONGO_SVC_CONNECTIONURI >/dev/null
+
+curl -H "X-Consul-Token: ${CONSUL_MASTER_TOKEN}" -s -X PUT ${HOST_DOMAIN}:8500/v1/kv/vamf/${VAMF_ENVIRONMENT}/apigateway/1.0/services/vista-emr-service -d '{  "location":"/VistaEmrService",  "service":"vista-emr-service",  "redirect":"off",  "request_headers":"on",  "headers": { "X-Real-IP":"$remote_addr", "X-Forwarded-Proto": "$scheme", "X-Forwarded-Host" : "$http_host" }}' > /dev/null
+
